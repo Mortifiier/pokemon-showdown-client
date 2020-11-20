1e9b99ca03747ecdf9768e821399cf26118b7f38
Mortifiier <74686542+Mortifiier@users.noreply.github.com>
2020-11-20 00:33:08 -0600

    Update client-mainmenu.js

--- js/client-mainmenu.js
+++ js/client-mainmenu.js
@@ -898,8 +898,8 @@
 			if (!noChoice) {
 				this.curFormat = formatid;
 				if (!this.curFormat) {
-					if (BattleFormats['gen8randombattle']) {
-						this.curFormat = 'gen8randombattle';
+					if (BattleFormats['rbanythinggoes']) {
+						this.curFormat = 'rbanythinggoes';
 					} else for (var i in BattleFormats) {
 						if (!BattleFormats[i].searchShow || !BattleFormats[i].challengeShow) continue;
 						this.curFormat = i;
