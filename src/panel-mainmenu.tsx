cdc81954bbbed4ae5c793595f817e8ad3113bfcd
Mortifiier <74686542+Mortifiier@users.noreply.github.com>
2020-11-19 23:54:05 -0600

    Update panel-mainmenu.tsx

--- src/panel-mainmenu.tsx
+++ src/panel-mainmenu.tsx
@@ -144,11 +144,8 @@ class MainMenuRoom extends PSRoom {
 				let teambuilderFormatName = '';
 				if (isTeambuilderFormat) {
 					teambuilderFormatName = name;
-					// if (id.slice(0, 3) !== 'gen') {
-					// 	teambuilderFormatName = '[Gen 6] ' + name;
-					// }
-					if (id.slice(0, 2) === 'rb') {
-						teambuilderFormatName = 'TEST' + name;
+					if (id.slice(0, 3) !== 'gen') {
+						teambuilderFormatName = '[Gen 6] ' + name;
 					}
 					let parenPos = teambuilderFormatName.indexOf('(');
 					if (parenPos > 0 && name.slice(-1) === ')') {
